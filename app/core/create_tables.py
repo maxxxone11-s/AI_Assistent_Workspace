82cb10c559eb498cb85457f0b57e21e74a462cd3
maxxxone11-s <maximzadko187@gmail.com>
2026-05-13 17:16:58 +0300

    4

--- app/core/create_tables.py
+++ app/core/create_tables.py
@@ -2,10 +2,10 @@ import asyncio
 
 from app.core.database import engine
 from app.core.base import Base
-
-DATABASE_URL = "postgresql+asyncpg://postgres:010SS@localhost:5432/ai_workspace"
+from app.models.user_model import User
 
 async def create_tables():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
     
+asyncio.run(create_tables())
--- app/core/database.py
+++ app/core/database.py
@@ -1,6 +1,7 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
-engine = create_async_engine("подключение к db")
+DATABASE_URL = "postgresql+asyncpg://postgres:010SS@localhost:5432/ai_workspace"
+engine = create_async_engine(DATABASE_URL)
 
 AsyncSessionLocal = async_sessionmaker(
     bind=engine,
